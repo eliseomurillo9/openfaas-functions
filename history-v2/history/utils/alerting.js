adc2508989328ac2a9431089d566f6e75dd47263
Eliseo Murillo <eliseomurillo9@gmail.com>
2026-06-01 21:40:58 +0200

    fix: correct alert message to display alerts instead of errors

--- history-v2/history/utils/alerting.js
+++ history-v2/history/utils/alerting.js
@@ -8,7 +8,7 @@ async function alerting(report) {
             from: 'onboarding@resend.dev',
             to: 'eliseomurillo9@gmail.com',
             subject: `Alerting: Sensor ${report.id}`,
-            html: `<h1>Threshold exceed for sensor ${report.id}</h1> </br><p>${report.erroars}</p>`
+            html: `<h1>Threshold exceed for sensor ${report.id}</h1> </br><p>${report.alerts}</p>`
         });
 
         if (error) throw error;
