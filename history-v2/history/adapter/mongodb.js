5b27c387164968dbcf755d9983239460970da9ac
Eliseo Murillo <eliseomurillo9@gmail.com>
2026-06-01 23:24:39 +0200

    feat: update MongoDB connection handling and modify test options

--- history-v2/history/adapter/mongodb.js
+++ history-v2/history/adapter/mongodb.js
@@ -1,5 +1,10 @@
 const { MongoClient } = require('mongodb');
+const uri = 'mongodb+srv://eliseomurillo9:jH57Kfrc4ka027QX@iot-tp.sfzmdp5.mongodb.net/';
+const client = new MongoClient(uri, {
+    maxPoolSize: 200
+});
 
+client.connect();
 async function save(alertBody, collection) {
     //TODO: Create a connection pool
     const uri = 'mongodb+srv://eliseomurillo9:jH57Kfrc4ka027QX@iot-tp.sfzmdp5.mongodb.net/';
@@ -13,8 +18,6 @@ async function save(alertBody, collection) {
     } catch (error) {
         console.error('Error saving alert to MongoDB:', error);
         throw error;
-    }finally {
-        await client.close();
     }
 }
 
